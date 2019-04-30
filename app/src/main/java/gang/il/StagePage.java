a14352500de93e314aaf8a30cb99cb50f5c03406
Lee-J-H <coin484@naver.com>
2019-04-30 19:50:16 +0900

    stage page 수정

--- app/src/main/java/gang/il/StagePage.java
+++ app/src/main/java/gang/il/StagePage.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import static gang.il.CheckedStage.onCheckStage;
 import static gang.il.Valiable.CLEAR_STAGE;
@@ -21,10 +22,10 @@ import static gang.il.Valiable.StagePage;
 import static gang.il.Valiable.LOAD_FINISH;
 import static gang.il.Valiable.stageCount;
 
-public class StagePage extends AppCompatActivity implements View.OnClickListener {
+public class StagePage extends AppCompatActivity {
     Context mContext = this;
-    LinearLayout[] stageNum = new LinearLayout[4];
-    ImageView[] lockImg;
+    ListView stageList;
+    StageList stageListAdapter = new StageList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,36 +33,8 @@ public class StagePage extends AppCompatActivity implements View.OnClickListener
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//제목 없음
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stage_page);
-        inti();
-        matchStage();
-    }
-
-    public void inti() {
-        for (int i = 0; i < stageNum.length; i++) {
-            int stageID = getResources().getIdentifier("stage_" + (i + 1), "id", "gang.il");
-            stageNum[i] = findViewById(stageID);
-            stageNum[i].setOnClickListener(this);
-        }
-    }
-
-    public void matchStage() {
-        int succeedStage = Integer.parseInt(onCheckStage(mContext));
-        lockImg = new ImageView[succeedStage + 1]; //클리어된 스테이지 확인
-        for (int i = 0; i < lockImg.length; i++) {
-            int stageID = getResources().getIdentifier("lock_" + (i + 1), "id", "gang.il");
-            lockImg[i] = findViewById(stageID);
-            lockImg[i].setVisibility(View.GONE);
-            stageNum[i].setBackgroundColor(Color.parseColor("#F4DFAC"));
-
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        stageCount = v.getTag().toString();
-        if (Integer.parseInt(stageCount) > lockImg.length) return;
-        LoadDB.GetDB Data = new LoadDB.GetDB();
-        Data.execute("http://106.10.57.117/EscapeFarm/getStage.php", stageCount);  //스테이지DB 로딩
+        stageList = findViewById(R.id.stage_list);
+        stageList.setAdapter(stageListAdapter);
     }
 
     final static Handler mhandler = new Handler() {
@@ -80,7 +53,7 @@ public class StagePage extends AppCompatActivity implements View.OnClickListener
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == CLEAR_STAGE) {
             if (resultCode == RESULT_OK) {
-                matchStage();
+                stageListAdapter.notifyDataSetChanged();
             }
         }
     }
