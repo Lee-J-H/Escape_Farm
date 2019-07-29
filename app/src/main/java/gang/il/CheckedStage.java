7e38b4a4fe4ecb7a94f2541fe143826997fe10a9
Lee-J-H <coin484@naver.com>
2019-07-29 12:05:33 +0900

    add blind mode

--- app/src/main/java/gang/il/CheckedStage.java
+++ app/src/main/java/gang/il/CheckedStage.java
@@ -5,20 +5,22 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import static gang.il.Valiable.gameMode;
+
 public class CheckedStage extends Activity {
     static String[] stageText; //스테이지 클리어 점수 저장 변수
+
     public static void onClearStage(Context context, int stage, int rating) {  //스테이지 완료시 저장
         StringBuilder stageTextBuf = new StringBuilder();
-        if(stage < stageText.length) {  //현재 저장된 마지막 스테이지 이전의 스테이지들을 완료한 경우
-            if (Integer.parseInt(stageText[stage]) < rating);  //저장된 점수 이상의 점수를 얻은 경우
+        if (stage < stageText.length) {  //현재 저장된 마지막 스테이지 이전의 스테이지들을 완료한 경우
+            if (Integer.parseInt(stageText[stage]) < rating) ;  //저장된 점수 이상의 점수를 얻은 경우
             stageText[stage] = String.valueOf(rating);
-            for(int i=0; i<stageText.length; i++) {
+            for (int i = 0; i < stageText.length; i++) {
                 stageTextBuf.append(stageText[i]);
-                if(i!=stageText.length)stageTextBuf.append(",");
+                if (i != stageText.length) stageTextBuf.append(",");
             }
-        }
-        else{ //현재 저장된 마지막 스테이지 다음의 스테이지를 완료한 경우
-            for(int i=0; i<stageText.length; i++) {
+        } else { //현재 저장된 마지막 스테이지 다음의 스테이지를 완료한 경우
+            for (int i = 0; i < stageText.length; i++) {
                 stageTextBuf.append(stageText[i]);
                 stageTextBuf.append(",");
             }
@@ -26,15 +28,22 @@ public class CheckedStage extends Activity {
         }
         SharedPreferences mode = context.getSharedPreferences("stageTest", MODE_PRIVATE);
         SharedPreferences.Editor editor = mode.edit();
-        editor.putString("stage", stageTextBuf.toString());
+        if (gameMode.equals("classic"))
+            editor.putString("stage_classic", stageTextBuf.toString());
+        else if (gameMode.equals("blind"))
+            editor.putString("stage_blind", stageTextBuf.toString());
         editor.commit();
-        stageTextBuf.delete(0,stageTextBuf.length());
+        stageTextBuf.delete(0, stageTextBuf.length());
     }
 
     public static int onCheckStage(Context context) { //현재까지 완료된 스테이지 확인
         SharedPreferences mode = context.getSharedPreferences("stageTest", MODE_PRIVATE);
         SharedPreferences.Editor editor = mode.edit();
-        String stage = mode.getString("stage", "0");
+        String stage = "0";
+        if (gameMode.equals("classic"))
+            stage = mode.getString("stage_classic", "0");
+        else if (gameMode.equals("blind"))
+            stage = mode.getString("stage_blind", "0");
         stageText = stage.split(",");
         return stageText.length;
     }//http://devstory.ibksplatform.com/2017/12/android-sharedpreferences.html
--- app/src/main/java/gang/il/MainPage.java
+++ app/src/main/java/gang/il/MainPage.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import static gang.il.Valiable.gameMode;
+
 public class MainPage extends AppCompatActivity {
 
     @Override
@@ -14,10 +16,20 @@ public class MainPage extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//제목 없음
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
-        Button stagebtn =(Button)findViewById(R.id.stage_btn);
-        stagebtn.setOnClickListener(new View.OnClickListener() {
+        Button classic_btn =(Button)findViewById(R.id.classic_btn);
+        classic_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                gameMode = "classic";
+                Intent intent = new Intent(MainPage.this,StagePage.class);
+                startActivity(intent);
+            }
+        });
+        Button blind_btn =(Button)findViewById(R.id.blind_btn);
+        blind_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameMode = "blind";
                 Intent intent = new Intent(MainPage.this,StagePage.class);
                 startActivity(intent);
             }
