d6ae7c522fce668c6ad56762286611c2669aa57e
Lee-J-H <coin484@naver.com>
2019-08-29 17:41:46 +0900

    update clearDialogUI & gamePageUI

--- app/src/main/java/gang/il/GamePage.java
+++ app/src/main/java/gang/il/GamePage.java
@@ -3,6 +3,7 @@ package gang.il;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ import android.widget.TextView;
 import static gang.il.LoadingImg.progressDialog;
 import static gang.il.LoadingImg.progressOFF;
 import static gang.il.LoadingImg.progressON;
+import static gang.il.Valiable.finishObj;
 import static gang.il.Valiable.gameMode;
 import static gang.il.Valiable.mContext;
 import static gang.il.Valiable.minCount;
@@ -29,6 +31,7 @@ import static gang.il.Valiable.dialog;
 public class GamePage extends AppCompatActivity {
 
     ImageView backBtn, resetBtn;
+    ImageView[] animalWidget = new ImageView[5];
     Activity GamePage = this;
 
     @Override
@@ -44,17 +47,13 @@ public class GamePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_page);
         init();
+        animalWidget();
     }
 
     public void init() {
         backBtn = (ImageView) findViewById(R.id.back);
         resetBtn = (ImageView) findViewById(R.id.reset);
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                backStage();
-            }
-        });
+        backBtn.setOnClickListener(backBtnListener);
         resetBtn.setOnClickListener(ResetListener);
         setMinCount();
         tutorialNum=1;
@@ -85,12 +84,50 @@ public class GamePage extends AppCompatActivity {
         progressON(GamePage,null);
         finish();
     }
+    public void animalWidget(){
+            animalWidget[0] = findViewById(R.id.widget_dog);
+            animalWidget[1] = findViewById(R.id.widget_squirrel);
+            animalWidget[2] = findViewById(R.id.widget_rabbit);
+            animalWidget[3] = findViewById(R.id.widget_panda);
+            animalWidget[4] = findViewById(R.id.widget_tiger);
+            for(int i=0; i<finishObj.size(); i++){
+                if(finishObj.get(i).equals("dog"))
+                    animalWidget[0].setVisibility(View.VISIBLE);
+                if(finishObj.get(i).equals("squirrel"))
+                    animalWidget[1].setVisibility(View.VISIBLE);
+                if(finishObj.get(i).equals("rabbit"))
+                    animalWidget[2].setVisibility(View.VISIBLE);
+                if(finishObj.get(i).equals("panda"))
+                    animalWidget[3].setVisibility(View.VISIBLE);
+                if(finishObj.get(i).equals("tiger"))
+                    animalWidget[4].setVisibility(View.VISIBLE);
+            }
+    }
+
+    public void finWidget(String animal){
+        switch (animal){
+            case "dog":
+                animalWidget[0].setImageDrawable(mContext.getResources().getDrawable(R.drawable.dog_widget));
+                break;
+            case "squirrel":
+                animalWidget[1].setImageDrawable(mContext.getResources().getDrawable(R.drawable.squirrel_widget));
+                break;
+            case "rabbit":
+                animalWidget[2].setImageDrawable(mContext.getResources().getDrawable(R.drawable.rabbit_widget));
+                break;
+            case "panda":
+                animalWidget[3].setImageDrawable(mContext.getResources().getDrawable(R.drawable.panda_widget));
+                break;
+            case "tiger":
+                animalWidget[4].setImageDrawable(mContext.getResources().getDrawable(R.drawable.tiger_widget));
+                break;
+        }
+    }
 
 
     public void Dialog() {
-        dialog = new StageClearDialog(mContext,
-                "스테이지 완료" + "\n이동횟수:" + moveCount + "\n점수:", // 내용
-                OKDialogListener, clr_ResetListener); // 왼쪽 버튼 이벤트
+        dialog = new StageClearDialog(mContext,backBtnListener, // 내용
+                nextDialogListener, clr_ResetListener); // 왼쪽 버튼 이벤트
         // 오른쪽 버튼 이벤트
 
         //요청 이 다이어로그를 종료할 수 있게 지정함
@@ -100,7 +137,15 @@ public class GamePage extends AppCompatActivity {
     }
 
     //다이얼로그 클릭이벤트
-    private View.OnClickListener OKDialogListener = new View.OnClickListener() {
+    private View.OnClickListener backBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            backStage();
+        }
+    };
+
+    //다이얼로그 클릭이벤트
+    private View.OnClickListener nextDialogListener = new View.OnClickListener() {
         public void onClick(View v) {
             progressON(GamePage,null);
             stageCount = String.valueOf(Integer.parseInt(stageCount)+1);
@@ -141,14 +186,4 @@ public class GamePage extends AppCompatActivity {
     };
 //출처: http://yoo-hyeok.tistory.com/51 [유혁의 엉터리 개발]
 
-    public int checkMinimumMove() {
-        int rating;
-        if (moveCount <= minCount)
-            rating = 10;
-        else if (moveCount < minCount + 2)
-            rating = 5;
-        else
-            rating = 1;
-        return rating;
-    }
 }
--- app/src/main/java/gang/il/StageClearDialog.java
+++ app/src/main/java/gang/il/StageClearDialog.java
@@ -11,18 +11,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static gang.il.Valiable.minCount;
+import static gang.il.Valiable.moveCount;
+import static gang.il.Valiable.stageCount;
+
 public class StageClearDialog extends Dialog {
 
-    private TextView mContentView;
+    /*private TextView mContentView;
     private TextView mBlankView1, mBlankView2, mBlankView3;
     private ImageButton mRightImageButton;
     private ImageButton mLeftImageButton;
-    private ImageView mDialogImg;
+    private ImageView mDialogImg;*/
     private String mContent;
     private LinearLayout.LayoutParams params;
+    private ImageView menuIcon,nextIcon,retryIcon;
+    private TextView clrStage,clrMin,clrMove;
 
     private View.OnClickListener mRightClickListener;
     private View.OnClickListener mLeftClickListener;
+    private View.OnClickListener menuClickListener;
+    private View.OnClickListener retryClickListener;
+    private View.OnClickListener nextClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class StageClearDialog extends Dialog {
 
         setContentView(R.layout.stage_clear_dialog);
 
-        mContentView = (TextView) findViewById(R.id.dialog_text);
+        /*mContentView = (TextView) findViewById(R.id.dialog_text);
         mRightImageButton = (ImageButton) findViewById(R.id.dialog_right_btn);
         mLeftImageButton = (ImageButton) findViewById(R.id.dialog_left_btn);
         mDialogImg = (ImageView) findViewById(R.id.dialog_img);
@@ -44,13 +53,27 @@ public class StageClearDialog extends Dialog {
         mBlankView2 = (TextView) findViewById(R.id.blank3);
         mBlankView3 = (TextView) findViewById(R.id.blank4);
         LinearLayout blank = (LinearLayout) findViewById(R.id.blank_layout);
-        LinearLayout btn_layout = (LinearLayout) findViewById(R.id.btn_layout);
+        LinearLayout btn_layout = (LinearLayout) findViewById(R.id.btn_layout); */
         // 제목과 내용을 생성자에서 셋팅한다.
-        mContentView.setText(mContent);
+
+        menuIcon = (ImageView) findViewById(R.id.menu_icon);
+        nextIcon = (ImageView) findViewById(R.id.next_icon);
+        retryIcon = (ImageView) findViewById(R.id.retry_icon);
+        clrStage = (TextView) findViewById(R.id.clr_stage);
+        clrMin = (TextView) findViewById(R.id.clr_min);
+        clrMove = (TextView) findViewById(R.id.clr_move);
+
+        //mContentView.setText(mContent);
 
         // 클릭 이벤트 셋팅
-
-        if (mLeftClickListener != null && mRightClickListener != null) { //버튼 2개짜리
+        menuIcon.setOnClickListener(menuClickListener);
+        nextIcon.setOnClickListener(nextClickListener);
+        retryIcon.setOnClickListener(retryClickListener);
+        clrStage.setText("stage:"+stageCount);
+        clrMove.setText("이동횟수:"+moveCount);
+        clrMin.setText("최소횟수:"+minCount);
+        //clrStage,clrMin,clrMove
+        /*if (mLeftClickListener != null && mRightClickListener != null) { //버튼 2개짜리
             mLeftImageButton.setOnClickListener(mLeftClickListener);
             mRightImageButton.setOnClickListener(mRightClickListener);
             mRightImageButton.setImageResource(R.drawable.dialog_no);
@@ -111,7 +134,7 @@ public class StageClearDialog extends Dialog {
 //[출처] [안드로이드] 뷰의 가중치(layout_weight) 코드에서 동적으로 변경|작성자 자바킹
         } else {
 
-        }
+        }*/
     }
 
 
@@ -132,4 +155,13 @@ public class StageClearDialog extends Dialog {
         this.mLeftClickListener = leftListener;
         this.mRightClickListener = rightListener;
     }
+
+    public StageClearDialog(Context context, View.OnClickListener menuListener,
+                            View.OnClickListener nextListener,View.OnClickListener retryListener) {
+        super(context, android.R.style.Theme_Translucent_NoTitleBar);
+        this.menuClickListener = menuListener;
+        this.nextClickListener = nextListener;
+        this.retryClickListener = retryListener;
+    }
 }
+
